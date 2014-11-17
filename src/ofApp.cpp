70ce971f9b02067dc10c4db9c48e8aa8223d0e25
luming <lh00000000@gmail.com>
2014-11-17 13:56:31 -0500

    weird makefile error

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,61 +1,106 @@
 #include "ofApp.h"
 
+
+using namespace scythe;
+using namespace std;
+
+
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+    y = seqa(5, 1, n);
+    X = Matrix<double>(n, 3, false);
+    X = 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 0, 1, 0, 1, 0;
+    poisson_model.y_ = y;
+    poisson_model.X_ = X;
+
+    theta = invpd(crossprod(X)) * t(X) * log(y);
+    beta_MLE = BFGS(poisson_model, theta, myrng, 100, 1e-5, true);
+    beta_bs_store = Matrix<double>(M, 3);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::update(){
+void ofApp::draw() {
+
+    if (i < M) {
+        Matrix<double> eta = X * beta_MLE;
+        Matrix<double> m = exp(eta);
+        for (int j = 0; j < n; ++j)
+            poisson_model.y_(j) = myrng.rpois(m(j));
+
+        beta_bs_store(i, _) = BFGS(poisson_model, beta_MLE, myrng, 100, 1e-5);
+        i++;
+    }
+
+
+
+
+    //cout << "The MLEs are: " << endl;
+    //std::cout << t(beta_MLE) << "\n";
+    //cout << "The bootstrap SEs are: " << endl;
+
+
+    //beta_bs_store is M x n matrix
+    for (int c = 0; c < beta_bs_store.cols(); c ++) {
+        //ofDrawBitmapString(sdc(beta_bs_store)(0,c), c*10 + 10, 10 * 10);
+        float top = ofMap(sdc(beta_bs_store)(0,c), 0,1, ofGetHeight(),0);
+        float left = ofMap(c, 0, beta_bs_store.cols(), 0, ofGetWidth());
+        ofSetColor(255);
+        ofFill();
+        ofRect(left, top, ofGetWidth()/beta_bs_store.cols(), ofGetHeight()-top);
+    }
+
+    //std::cout<<sdc(beta_bs_store)<< "\n";
+
+
 
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::keyPressed(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::keyReleased(int key) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyReleased(int key){
+void ofApp::mouseMoved(int x, int y ) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::mouseDragged(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
+void ofApp::windowResized(int w, int h) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
+void ofApp::gotMessage(ofMessage msg) {
 
 }
 
 //--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
+void ofApp::dragEvent(ofDragInfo dragInfo) {
 
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -2,21 +2,69 @@
 
 #include "ofMain.h"
 
-class ofApp : public ofBaseApp{
+#include <scythestat/rng/mersenne.h>
+#include <scythestat/distributions.h>
+#include <scythestat/ide.h>
+#include <scythestat/la.h>
+#include <scythestat/matrix.h>
+#include <scythestat/rng.h>
+#include <scythestat/smath.h>
+#include <scythestat/stat.h>
+#include <scythestat/optimize.h>
+#include <string>
+#include <iostream>
 
-	public:
-		void setup();
-		void update();
-		void draw();
+using namespace std;
+using namespace scythe;
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-		
+
+class PoissonModel {
+public:
+    double operator() (const Matrix<double> beta) {
+        const int n = y_.rows();
+        Matrix<double> eta = X_ * beta;
+        Matrix<double> m = exp(eta);
+        double loglike = 0.0;
+        for (int i=0; i<n; ++i)
+            loglike += y_(i) * log(m(i)) - m(i);
+        return -1.0 * loglike;
+    }
+    Matrix<double> y_;
+    Matrix<double> X_;
+};
+
+class ofApp : public ofBaseApp {
+
+public:
+    void setup();
+    void update();
+    void draw();
+
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+
+    mersenne myrng;
+	const int n = 5;
+	Matrix<double> y;
+	Matrix<double> X;
+
+	PoissonModel poisson_model;
+
+	Matrix<double> theta;
+	Matrix<double> beta_MLE;
+
+	const int M = 10000;
+	Matrix<double> beta_bs_store;
+
+	int i = 0;
+
+    ostringstream s;
+
 };
